f024240c720891be255a99743b0b825e039bc0a0
krak-ken <aniketsahu899@gmail.com>
2021-09-15 16:56:24 +0530

    Added POJOs

---
+++ src/main/java/course/project/pet_clinic/model/Owner.java
@@ -0,0 +1,5 @@
+package course.project.pet_clinic.model;
+
+public class Owner extends Person {
+    
+}
---
+++ src/main/java/course/project/pet_clinic/model/Vet.java
@@ -0,0 +1,5 @@
+package course.project.pet_clinic.model;
+
+public class Vet extends Person {
+    
+}
