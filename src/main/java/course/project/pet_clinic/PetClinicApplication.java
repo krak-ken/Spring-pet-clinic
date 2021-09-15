e3d900c3cc64163f89a3f8627d2d707b7e8e6642
krak-ken <aniketsahu899@gmail.com>
2021-09-15 16:47:27 +0530

    Project Setup.

---
+++ src/main/java/course/project/pet_clinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package course.project.pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
